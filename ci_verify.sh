3631a52877b4e8449a80a4fd25cc9542ac231b57
Giles Knap <giles.knap@diamond.ac.uk>
2023-11-06 14:52:42 +0000

    update CI to validate IOC instances

--- ci_verify.sh
+++ ci_verify.sh
@@ -7,26 +7,11 @@ set -ex
 
 for ioc in iocs/*
 do
+    # Skip if subfolder is not an IOC definition
     if [ ! -d "${ioc}/config" ]; then
         continue
     fi
 
-    # verify that the instance can generate a schema
-    ec -d dev launch --target runtime ${ioc} --execute \
-    'if [[ -d /epics/ibek/ ]]; then ibek ioc generate-schema /epics/ibek/*.ibek.support.yaml; fi'
-
-    # verify that the instance can launch its IOC
-
-    # put the values.yaml file in a test config directory with basic startup script
-    cp -r ${ioc}/values.yaml ci_test/
-
-    # launch the generic IOC pointing at that config
-    # TODO this is not really testing any Python IOCs so need to think about that
-    ec -d dev launch --target runtime ci_test --args '-dit'
-
-    # a python IOC will immediately exit.
-    # Docker errors stopping a non-existent container unlike podman
-    ec -d dev stop || echo docker stop failed
-
+    ec ioc validate "${ioc}"
 done
 
